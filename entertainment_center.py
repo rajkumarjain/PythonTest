a3f20abdcda8a8fc4dd9b148c2eb327d91d66f5e
rajkumarjain <onlyrajkumar@gmail.com>
2021-08-22 19:37:07 +0530

    Fileadded

---
+++ entertainment_center.py
@@ -0,0 +1,44 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=JcpWXaA2qeg")
+
+#print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+                     "A marine on an aline planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=EzETGqZN6dU")
+# print(avatar.storyline)
+# avatar.show_trailer()
+
+school_of_rock = media.Movie("School of Rock",
+                             "Using rock music to learn",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Rataouille",
+                          "Storyline",
+                          "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                          "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+midnight_in_paris = media.Movie("Midnight in paris",
+                                "Using rock music to learn",
+                                "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                                "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+hunger_games = media.Movie("Hunger games"
+                           "",
+                           "Using rock music to learn",
+                           "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                           "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+# All Movies On in a list
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+# Call the template & open the site
+fresh_tomatoes.open_movies_page(movies)
+#print(media.Movie.VALID_RATINGS)
+#print(media.Movie.__doc__)
